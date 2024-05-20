5fc24f1de67be219e527f53580d3b13ac634fe48
Md Golam Moctader <golam.moctader@gmail.com>
2024-05-20 13:41:41 +0300

    added comments

--- app.py
+++ app.py
@@ -10,34 +10,21 @@ app = Flask(__name__)
 with app.app_context():
     # Init code here
     print(f'Flask app running on port {PORT}')
+    print('change 1')
 
 @app.route("/")
 def index():
     print(f'{request.method} request from {request.remote_addr}')
     ret = {
-        'msg': 'Rahti Flask demo works.', 
+        'msg': 'Rahti Flask demo works.',
+        'port': PORT,   
         'env': os.environ.get('ENV_VAR', 'not working'), 
         'method': request.method
     }
 
     return ret
 
-@app.route("/test-route", methods = [ 'GET', 'POST' ])
-def test_route():
 
-    if request.method == 'POST':
-    
-        try:
-            ret =  { 'msg': 'POST works', 'postdata': request.get_json() }
-
-        except Exception as e:
-            print(e)
-            ret =  { 'msg': 'ERROR: POST failed.' }, 500
-
-    else:
-        ret = {'msg': 'Test route GET'}
-
-    return ret
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT, host='0.0.0.0' )
